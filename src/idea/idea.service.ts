f821db8b24d57072ab2f59dcfdc8b9a2e83935f0
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-29 18:35:36 -0400

    created basic crudf service

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+
+import { IdeaEntity } from "./idea.entity";
 
 @Injectable()
-export class IdeaService {}
+export class IdeaService {
+  constructor(
+    @InjectRepository(IdeaEntity) private ideaRepository: Repository<IdeaEntity>
+  ) {}
+
+  async showAll() {
+    return await this.ideaRepository.find();
+  }
+
+  async create() {
+    const idea = await this.ideaRepository.create(data);
+    await this.ideaRepository.save(idea);
+    return idea;
+  }
+
+  async read(id: string) {
+    return await this.ideaRepository.findOne({
+      where: { id }
+    });
+  }
+
+  async update(id: string, data) {
+    await this.ideaRepository.update(
+      {
+        id
+      },
+      data
+    );
+    return await this.ideaRepository.findOne({
+      id
+    });
+  }
+
+  async destroy(id: string) {
+    await this.ideaRepository.delete({ id });
+    return { deleted: true };
+  }
+}
