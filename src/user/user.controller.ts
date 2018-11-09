aa578c71e47a4663f74558efee2ffbb620d27ee5
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-09 14:34:17 -0500

    created user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from "@nestjs/common";
+import { UserService } from "./user.service";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get("api/users")
+  showAllUsers() {
+    return this.userService.showAll();
+  }
+
+  @Post("login")
+  login(@Body() data) {
+    return this.userService.login(data);
+  }
+
+  @Post("register")
+  register(@Body() data) {
+    return this.userService.register(data);
+  }
+}
